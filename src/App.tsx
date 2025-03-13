7f8ede675ee76791ecdc4cdd00d3f6498e3dd36c
GLiklikadze <102631687+GLiklikadze@users.noreply.github.com>
2025-03-13 13:46:00 +0400

    add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router";
 import RootLayout from "@/components/layout/RootLayout";
+import { Spinner } from "@/components/ui/loading/LoadingSpinner";
+import CreatePage from "@/pages/create-task/CreatePage";
 const TasksPage = lazy(() => import("@/pages/tasks/TasksPage"));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<RootLayout />}>
-        <Route index element={<TasksPage />} />
+        <Route
+          index
+          element={
+            <Suspense fallback={<Spinner size="large" />}>
+              <TasksPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="create-task"
+          element={
+            <Suspense fallback={<Spinner size="large" />}>
+              <CreatePage />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
---
+++ src/pages/create-task/CreatePage.tsx
@@ -0,0 +1,9 @@
+const CreatePage = () => {
+  return (
+    <h2 className="text-[2.13rem] font-semibold text-red-200">
+      შექმენი ახალი დავალება
+    </h2>
+  );
+};
+
+export default CreatePage;
